efdf0d08d6928b27dda1f051f7fb5a25e91d99ec
Patatina <estudioh1chile@gmail.com>
2023-07-30 01:24:20 -0400

    tercer challenge

--- ProductManager.js
+++ ProductManager.js
@@ -1,143 +1,119 @@
-const fs = require('fs');
+import fs from 'fs';
 
 class ProductManager {
     constructor(path) {
-        this.product=[]
-        this.path = path= "\products.json";
-    }
+        this.product = [];
+        this.path = path || './products.json';
+            }
 
     async getProducts() {
         try {
-            let products = await fs.promises.readFile(this.path, "utf8");
-            console.log(products);
-            return JSON.parse(products);
+            let productsData = await fs.promises.readFile(this.path, "utf8");
+            this.product = JSON.parse(productsData).map(product => ({
+                ...product,
+                id: parseInt(product.id) // Convertir el id a número
+            }));
+            return this.product;
         } catch (error) {
             console.error(error);
+            throw error;
         }
     }
 
     async addProduct(title, description, price, thumbnail, code, stock) {
         try {
-            if (!title || !description || !price || !thumbnail || !code || !stock) throw new Error('All fields are required');
-            //let products = await this.getProducts();
-            let maxId = 0;
-            if (!this.product == []){
-                if (this.product.some(product => product.code === code)) throw new Error("Code already exists.");
-                
-               // products.forEach(product => { if (product.id > maxId) maxId = product.id} );
+            if (!title || !description || !price || !thumbnail || !code || !stock) {
+                throw new Error('All fields are required');
             }
+    
+            let maxId = 0;
+            if (this.product.length !== 0) {
+                if (this.product.some(product => product.code === code)) {
+                    throw new Error("Code already exists.");
+                }
+            }
+            
             maxId++; 
-            let newProduct = { id:this.addNewId(), title, description, price, thumbnail, code, stock };
+            let newProduct = { id: this.addNewId(), title, description, price, thumbnail, code, stock };
             this.product.push(newProduct);
             console.log(this.product);
             await fs.promises.writeFile(this.path, JSON.stringify(this.product));
-
-            
         } catch (error) {
             console.error(error);
+            throw error;
         }
     }
-    addNewId () {
-        return this.product.length +1;
+
+    addNewId() {
+        return this.product.length + 1;
     }
 
     async getProductById(id) {
         try {
             let products = await this.getProducts();
-            let product = products.find(product => product.id === id);
-            if (!product) throw new Error("Not found.");
-            console.log(product);
+            let product = products.find(product => product.id === Number(id));
+            if (!product) {
+                throw new Error("Not found.");
+            }
             return product;
         } catch (error) {
-            console.error(error)
+            console.error(error);
+            throw error;
         }
     }
 
     async updateProduct(id, { title, description, price, thumbnail, code, stock }) {
         try {
-          let products = await this.getProducts();
-          let index = products.findIndex((product) => product.id === id);
-          if (index === -1) throw new Error("Not found.");
-          let productToUpdate = { id, title, description, price, thumbnail, code, stock };
-          products[index] = productToUpdate;
-          await fs.promises.writeFile(this.path, JSON.stringify(products));
-          console.log(products);
-          return console.log("Successfully updated");
+            let products = await this.getProducts();
+            const productId = parseInt(id); // Convertir el ID a número
+
+            const productIndex = products.findIndex(product => product.id === productId);
+
+            if (productIndex === -1) {
+                throw new Error("Product not found.");
+            }
+
+            if (products.some(otherProduct => otherProduct.code === code && otherProduct.id === productId)) {
+                throw new Error("Code already exists.");
+            }
+
+            // Actualizar los datos del producto en la lista
+            products[productIndex] = {
+                ...products[productIndex],
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock
+            };
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products));
+
+            console.log("Successfully updated");
+            return products[productIndex]; // Devolver el producto actualizado
         } catch (error) {
-          console.error(error);
+            console.error(error);
+            throw error;
         }
-      }
-      
+    }
+    
     async deleteProduct(id) {
         try {
             let products = await this.getProducts();
             let index = products.findIndex(product => product.id === id);
-            if (index === -1) throw new Error("Not found.")
+            if (index === -1) {
+                throw new Error("Not found.");
+            }
             products.splice(index, 1);
             await fs.promises.writeFile(this.path, JSON.stringify(products));
             console.log(products);
-            return console.log("successfully deleted");
+            return console.log("Successfully deleted");
         } catch (error) {
             console.error(error);
+            throw error;
         }
     }
 }
 
-
-let productManager = new ProductManager("product.json");
-
-productManager.addProduct(
-  "producto prueba",
-  "Este es un producto prueba",
-  200,
-  "Sin imagen",
-  "abc126",
-  25
-);
-
-productManager.addProduct(
-  "producto prueba 2 ",
-  "Este es un producto prueba",
-  200,
-  "Sin imagen",
-  "kjbc156",
-  25
-);
-
-console.log(productManager.getProducts());
-
-productManager.addProduct(
-  "producto prueba 4",
-  "Este es un producto prueba",
-  200,
-  "Sin imagen",
-  "abc129903",
-  25
-);
-productManager.addProduct(
-    "producto prueba 6",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc129803",
-    25
-  );
-console.log(productManager.getProducts());
- productManager.getProductById(3).then(product => console.log(product));
-
-productManager
-    .updateProduct(3,{
-        title: "New title",
-        description: "New description",
-        price: "New price",
-        thumbnail: "New thumbnail",
-        code: "abc126",
-        stock: "800",
-    })
-    .then((product) => console.log(product))
-    .catch((error) => console.error(error));
-
-productManager
-    .deleteProduct(2)
-    .then(() => console.log("Product deleted"))
-    .catch((error) => console.error(error));
+export default ProductManager;
